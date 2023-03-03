eb43a6e54544044a6ec8b23647822d5e19cd7674
122pg0015 <122pg0015@yahoo.co.jp>
2023-03-03 17:33:29 +0900

    STAN

--- game/sorce/bossKnight.cpp
+++ game/sorce/bossKnight.cpp
@@ -47,12 +47,12 @@ bool	BossKnight::Process()
 
 	if (status == STATUS::STAN) {
 		animChange(BOSS1_hidan, &_modelInf, true, true, false);
-		if (time == 0) {
+		if (stanTime == 0) {
 			status = STATUS::WAIT;
-			_statusInf.stanPoint = 200;
 			stanTime = 100;
+			_statusInf.stanPoint = 0;
 		}
-		else { time--; }
+		else { stanTime--; }
 		return true;
 	}
 
@@ -193,7 +193,7 @@ bool	BossKnight::Process()
 		if (isAnimEnd == true) {
 			ActionFlag = false;
 			if (attackStep < 8)
-			{    
+			{
 				RangeJ();
 				if (attackStep == 1 && (rand() % 100) < 50)
 				{
@@ -278,7 +278,7 @@ bool	BossKnight::Process()
 				if (_modelInf.vec.y > 0 && attackStep == 1) { ActionFlag = true; }
 				else if (!isGround && attackStep == 2) { ActionFlag = true; }
 				else { attackStep++; }
-				
+
 			}
 			else {
 				UtilityJudge();
@@ -550,7 +550,7 @@ void BossKnight::Move(float speed, float radian) {
 bool BossKnight::HPmath(float Num,float Stan)
 {
 	_statusInf.hitPoint += Num;
-	_statusInf.stanPoint += Stan;
+	_statusInf.stanPoint -= Num;
 	if (Num <= -200) {
 		status = STATUS::DAMEGE;
 		ActionFlag = false;
