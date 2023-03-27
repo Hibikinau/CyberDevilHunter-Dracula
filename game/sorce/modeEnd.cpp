d6e80785f8a0d6666e9a536cf853939df06b03f4
riku yamamoto <raidennhibiki@yahoo.co.jp>
2023-03-27 14:52:41 +0900

    エンディング追加

---
+++ game/sorce/modeEnd.cpp
@@ -0,0 +1,28 @@
+#include"allMode.h"
+
+bool	modeE::Initialize()
+{
+	modeT::save("game/res/save.csv", &_modeServer->_valData);
+
+	return true;
+}
+
+bool	modeE::Process()
+{
+	if (_imputInf._gTrgb[KEY_INPUT_RETURN] || _imputInf._gTrgp[XINPUT_BUTTON_A])
+	{
+		_modeServer->Add(std::make_unique<modeT>(_modeServer), 1, MODE_TITLE);
+	}
+	return true;
+}
+
+bool	modeE::Render()
+{
+	DrawString(0, 0, "gameClear!!", GetColor(255, 255, 255));
+	return true;
+}
+
+bool	modeE::Terminate()
+{
+	return true;
+}
