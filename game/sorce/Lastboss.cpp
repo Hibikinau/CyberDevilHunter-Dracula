d185206d0474dd64ba4090de1b7ecc256e9ba2a0
122pg0017@tokyo.amg.ac.jp <122pg0017@tokyo.amg.ac.jp>
2023-04-18 20:04:19 +0900

    hai

--- game/sorce/Lastboss.cpp
+++ game/sorce/Lastboss.cpp
@@ -317,17 +317,17 @@ bool	LastBoss::Process()
 				if (status != STATUS::quick) { break; }
 			}
 		}
-		if (attackStep == 0 || attackStep == 2 || attackStep == 4)
+		if (attackStep == 0 || attackStep == 2 || attackStep == 4||attackStep==8)
 		{
 			makeAttackCap(VGet(0.f, 0.f, 0.f), VGet(0.f, 0.f, 100.f), 20.f, 10.f, _modelInf.totalTime * AwakeSpd - 10.f, animSpd, true, 20.f * AwakeDmg, 0, rWeponParentFrame, VGet(0, 0, 0), 1);
 			PlaySoundMem(swingSE, DX_PLAYTYPE_BACK);
 		}
 		if (ActionFlag == true) { break; }
-		if (attackStep == 7) { 
+		/*if (attackStep == 7) { 
 			_modelInf.dir = plMI->dir;
 			_modelInf.pos.x = plMI->pos.x;
 			_modelInf.pos.z = plMI->pos.z - 100;
-		}
+		}*/
 		animSpd = 3.0f * AwakeSpd;
 		animChange(PL_jaku_1 + attackStep , &_modelInf, false, false, true);
 		makeAttackCap(VGet(0.f, 0.f, 0.f), VGet(0.f, 0.f, 0.f), 10.f, .0f, _modelInf.totalTime * AwakeSpd, animSpd, true, 50.f * AwakeDmg, 0, 3, VGet(0, 0, 0), 1);
@@ -337,7 +337,7 @@ bool	LastBoss::Process()
 	case STATUS::STAB:
 		if (isAnimEnd == true) {
 			ActionFlag = false;
-			if (attackStep < 2) { 
+			if (attackStep < 3) { 
 				attackStep++; }
 			else {
 				UtilityJudge();
@@ -353,11 +353,18 @@ bool	LastBoss::Process()
 				/*makeAttackCap(VGet(0.f, 0.f, 0.f), VGet(0.f, 0.f, 100.f), 20.f, 10.f, _modelInf.totalTime * AwakeSpd - 10.f, animSpd, true, 20.f * AwakeDmg, 0, rWeponParentFrame, VGet(0, 0, 0), 1);
 				PlaySoundMem(swingSE, DX_PLAYTYPE_BACK);*/
 			}
+			if (attackStep == 3) {
+				if (_modelInf.playTime > 5) { Move(30.0f * AwakeMove, .0f); }
+			}
 			break;
 		}
-		animSpd = 1.0f * AwakeSpd;
+		if (attackStep == 1) {
+			animSpd = 2.0f * AwakeSpd;
+		}
+		else if(attackStep==2){ animSpd = 3.5f * AwakeSpd; }
+		else { animSpd = 1.2f * AwakeSpd; }
 		animChange(Boss_arts_tsuki_1 +attackStep ,&_modelInf, false, false, true);
-		if (attackStep == 2)
+		if (attackStep == 2 || attackStep == 3)
 		{
 			_modelInf.dir.y = Pdir;
 			makeAttackCap(VGet(0.f, 0.f, 0.f), VGet(0.f, 0.f, 100.f), 20.f, 10.f, _modelInf.totalTime* AwakeSpd - 10.f, animSpd, true, 20.f * AwakeDmg, 0, rWeponParentFrame, VGet(0, 0, 0), 1);
