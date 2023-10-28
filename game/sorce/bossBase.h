5f319e5c011860ac9ab2a10b953321d25957855b
122pg0017 <122pg0017@tokyo.amg.ac.jp>
2023-10-28 14:25:01 +0900

    ヘッダー仮組

--- game/sorce/bossBase.h
+++ game/sorce/bossBase.h
@@ -3,6 +3,20 @@
 
 class BossBase : public CharBase 
 {
+	enum class RANGE {
+		NONE,
+		CrossRange,
+		MidRange,
+		LongRange
+	};
+	RANGE range;
 
+	virtual enum class STATUS {
+		NONE
+	};
+	STATUS status;
 
+	bool loadJson(){}
+	RANGE RangeJ() { return range; }
+	STATUS UtilityJudge() {}
 };
